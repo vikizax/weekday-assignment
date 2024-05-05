094acbcad25d831aa2b8de138e87d99cabebad85
Vivek <vikichet@gmail.com>
2024-05-06 00:29:24 +0530

    added mui theme provider && updated font family

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material'
 import './index.css'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Lexend',
+      'sans-serif'
+    ].join(','),
+  }
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
